fac9ad2da06f42d09e93bafdb2440187b9ef13d1
PaulCHS <123192677+PaulCHS@users.noreply.github.com>
2023-03-22 09:13:15 -0700

    Made players and a tilemap and started other stuff.

---
+++ tilemap.g.ts
@@ -0,0 +1,43 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100003020202020202020202020202020204090101010101010101010101010101070901010101010101010101010101010709010101010101010101010101010107090101010101010101010101010101070901010101010101010101010101010709010101010101010101010101010107090101010101010101010101010101070901010101010101010101010101010709010101010101010101010101010107090101010101010101010101010101070901010101010101010101010101010709010101010101010101010101010107090101010101010101010101010101070901010101010101010101010101010706080808080808080808080808080805`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath5,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterWest0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
